0294f2f9e92b435d82dd74e7d56d319602d117c2
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2025-03-04 04:32:52 -0800

    Delete components directory

--- components/Users/utils/createImage.js
+++
@@ -1,9 +0,0 @@
-export function createImage(url) {
-  return new Promise((resolve, reject) => {
-    const img = new Image();
-    img.crossOrigin = "anonymous";
-    img.src = url;
-    img.onload = () => resolve(img);
-    img.onerror = (error) => reject(error);
-  });
-}
